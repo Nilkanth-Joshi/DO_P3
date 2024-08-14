3f5adcf3d07330412d1105bdf3bfecfa8e24b5f7
Nilkanth <hasti.mungra114579@marwadiuniversity.ac.in>
2024-08-14 09:27:37 +0530

    add

---
+++ src/main/java/com/Tc6B/app/App.java
@@ -0,0 +1,13 @@
+package com.Tc6B.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
